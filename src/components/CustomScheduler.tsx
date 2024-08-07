83a5fa55827cdd8b0bf01f85a544f4bf5fba0f09
Emil Olejnik <main@emilolejnik@gmail.com>
2024-08-07 23:14:27 +0200

    Feature: add local date switching

--- src/components/CustomScheduler.tsx
+++ src/components/CustomScheduler.tsx
@@ -1,20 +1,27 @@
 import { ViewTypes } from "@/types/CustomScheduler";
 import { SchedulerProps, ViewState } from "@devexpress/dx-react-scheduler";
-import { Appointments, AppointmentTooltip, DayView, MonthView, Scheduler, WeekView } from "@devexpress/dx-react-scheduler-material-ui";
+import { Appointments, AppointmentTooltip, CurrentTimeIndicator, DateNavigator, DayView, MonthView, Scheduler, Toolbar, WeekView, ViewSwitcher } from "@devexpress/dx-react-scheduler-material-ui";
 import { Box } from "@mui/material";
 interface CustomSchedulerProps extends Pick<SchedulerProps, 'data'> {
     currentViewName: ViewTypes
+    currentDate: Date
+    viewSwitchHandler: (viewName: string) => void
+    dateSwitchHandler: (date: Date) => void
 }
-function CustomScheduler({ data, currentViewName }: CustomSchedulerProps) {
+function CustomScheduler({ data, currentViewName, currentDate, viewSwitchHandler, dateSwitchHandler }: CustomSchedulerProps) {
     return (
         <Box>
             <Scheduler data={data} height={360} locale='pl-PL' firstDayOfWeek={1}>
-                <ViewState currentDate={'2024-11-01'} currentViewName={currentViewName}/>
-                <DayView />
-                <WeekView />
-                <MonthView />
+                <ViewState currentDate={currentDate} currentViewName={currentViewName} onCurrentViewNameChange={viewSwitchHandler} onCurrentDateChange={dateSwitchHandler}/>
+                <DayView displayName="Dzień"/>
+                <WeekView displayName="Tydzień"/>
+                <MonthView displayName="Miesiąc"/>
                 <Appointments />
                 <AppointmentTooltip />
+                <Toolbar />
+                <DateNavigator />
+                <ViewSwitcher />
+                <CurrentTimeIndicator updateInterval={60000} />
             </Scheduler>
         </Box>
     );
--- src/components/SchedulerCard.tsx
+++ src/components/SchedulerCard.tsx
@@ -1,31 +1,28 @@
-import { Card, CardContent, CardHeader, Divider, SelectChangeEvent } from "@mui/material"
+import { Card, CardContent, CardHeader, Divider } from "@mui/material"
 import CustomScheduler from "@/components/CustomScheduler"
-import CustomSelect from "@/components/UI/CustomSelect"
 import { AppointmentModel } from "@devexpress/dx-react-scheduler"
 import { useState } from "react"
 import { ViewTypes } from "@/types/CustomScheduler"
-import { SelectValues } from "@/types/SchedulerCard"
+
 const testData: AppointmentModel[] = [
     { startDate: '2024-11-01T09:45', endDate: '2024-11-01T11:00', title: 'Meeting' },
     { startDate: '2024-11-01T12:00', endDate: '2024-11-01T13:30', title: 'Go to a gym' }
 ]
-const possibleValues: SelectValues<ViewTypes> = [
-    { name: 'Day', displayName: 'Dzień' },
-    { name: 'Week', displayName: 'Tydzień' },
-    { name: 'Month', displayName: 'Miesiąc' },
-]
 function SchedulerCard(){
     const [currentViewName, setCurrentViewName] = useState<ViewTypes>('Day')
-    const viewSwitchHandler = (event: SelectChangeEvent) => {
-        setCurrentViewName(event.target.value as ViewTypes)
+    const [currentDate, setCurrentDate] = useState<Date>(new Date())
+    const viewSwitchHandler = (viewName: string) => {
+        setCurrentViewName(viewName as ViewTypes)
+    }
+    const dateSwitchHandler = (date: Date) => {
+        setCurrentDate(date)
     }
     return (
         <Card sx={{ border: '1px solid #ccc', maxWidth: '1440px' }}>
             <CardHeader title="Planer" sx={{ paddingBottom: '0' }}/>
             <CardContent>
-                <Divider sx={{ marginBottom: '1em' }}/>
-                <CustomSelect possibleValues={possibleValues} selectedValue={currentViewName} onChange={viewSwitchHandler}/>
-                <CustomScheduler data={testData} currentViewName={currentViewName} />
+                <Divider/>
+                <CustomScheduler data={testData} currentViewName={currentViewName} currentDate={currentDate} dateSwitchHandler={dateSwitchHandler} viewSwitchHandler={viewSwitchHandler} />
             </CardContent>
         </Card>
     )
