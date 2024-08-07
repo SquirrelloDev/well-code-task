a40d002dc0d52b7e575ba911caa326909c606b2a
Emil Olejnik <main@emilolejnik@gmail.com>
2024-08-07 20:45:01 +0200

    Feature: add simple header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import { Box } from "@mui/material";
+
+function Header() {
+    return (
+        <Box component='section' sx={{ padding: '1em', backgroundColor: '#ddd' }}>
+            <h1>Mój planer dnia</h1>
+        </Box>
+    );
+}
+export default Header
