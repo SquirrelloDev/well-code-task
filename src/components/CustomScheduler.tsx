31021621ec7398b29c2ec70370bd011192e0a8df
Emil Olejnik <main@emilolejnik@gmail.com>
2024-08-07 22:19:11 +0200

    Refactor: lift state up

--- src/components/CustomScheduler.tsx
+++ src/components/CustomScheduler.tsx
@@ -1,16 +1,18 @@
+import { ViewTypes } from "@/types/CustomScheduler";
+import { SchedulerProps, ViewState } from "@devexpress/dx-react-scheduler";
+import { Appointments, AppointmentTooltip, DayView, MonthView, Scheduler, WeekView } from "@devexpress/dx-react-scheduler-material-ui";
 import { Box } from "@mui/material";
-import { Appointments, AppointmentTooltip, Scheduler, WeekView } from "@devexpress/dx-react-scheduler-material-ui";
-import { AppointmentModel, ViewState } from "@devexpress/dx-react-scheduler";
-const testData: AppointmentModel[] = [
-    { startDate: '2024-11-01T09:45', endDate: '2024-11-01T11:00', title: 'Meeting' },
-    { startDate: '2024-11-01T12:00', endDate: '2024-11-01T13:30', title: 'Go to a gym' }
-]
-function CustomScheduler() {
+interface CustomSchedulerProps extends Pick<SchedulerProps, 'data'> {
+    currentViewName: ViewTypes
+}
+function CustomScheduler({ data, currentViewName }: CustomSchedulerProps) {
     return (
         <Box>
-            <Scheduler data={testData} height={360} locale='pl-PL' firstDayOfWeek={1}>
-                <ViewState currentDate={'2024-11-01'}/>
+            <Scheduler data={data} height={360} locale='pl-PL' firstDayOfWeek={1}>
+                <ViewState currentDate={'2024-11-01'} currentViewName={currentViewName}/>
+                <DayView />
                 <WeekView />
+                <MonthView />
                 <Appointments />
                 <AppointmentTooltip />
             </Scheduler>
